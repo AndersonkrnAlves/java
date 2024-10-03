db9328096a1763858211af162d71839f8dc29e01
AndersonkrnAlves <spfc-alves@hotmail.com>
2024-10-03 14:02:37 -0300

    salvar

--- src/Dados/Pessoa.java
+++ src/Dados/Pessoa.java
@@ -1,10 +1,28 @@
 package Dados;
 
+import javax.xml.catalog.GroupEntry.PreferType;
+
 public class Pessoa {
     
-    public String nome;
-    public String telefone;
-    public int idade;
+    private String nome;
+    private String telefone;
+    private int idade;
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String noome){
+        this.nome = nome;
+    }
 
     public Pessoa() {
         idade = 0;
@@ -16,11 +34,6 @@ public class Pessoa {
         nome = nom;
         telefone = tel;
         idade = id;
-    }
-    public Pessoa(Pessoa nova){
-        this.nome = nova.nome;
-        this.idade = nova.idade;
-        this.telefone = nova.telefone;
     }
     
     public boolean testeIdade(int idade) {
@@ -36,6 +49,12 @@ public class Pessoa {
         String retorno = "Nome: " + nome + " Idade: " + idade + " Telefone: " + telefone;
         return retorno;
     }
+    public Pessoa(Pessoa nova){
+        this.nome = nova.nome;
+        this.idade = nova.idade;
+        this.telefone = nova.telefone;
+    }
 }
+
 
 
